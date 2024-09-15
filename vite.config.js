b97a013b0a4021637f91011d163e461ff1e012f4
troonsply <troonsply@gmail.com>
2024-09-15 20:23:10 +0700

    Deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/chatbot/',  // Убедитесь, что путь совпадает с вашим репозиторием
+  base: '/chatbot/',
   build: {
     outDir: 'dist'
   }

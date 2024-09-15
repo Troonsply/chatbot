bb5b133ae52f833adb2d0d4de1ae47d92e9c00b9
troonsply <troonsply@gmail.com>
2024-09-15 19:56:20 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/chatbot/',  // Убедитесь, что путь совпадает с вашим репозиторием
+  build: {
+    outDir: 'dist'
+  }
 })
